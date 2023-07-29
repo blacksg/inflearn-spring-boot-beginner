31f86f5d0f503fbecde8977eedb17f52c76c2d02
blacksg <sagwa.nth@gmail.com>
2023-07-29 22:36:12 +0900

    Initial commit

---
+++ src/main/java/kamang/inflearnspringbootbeginner/InflearnSpringBootBeginnerApplication.java
@@ -0,0 +1,13 @@
+package kamang.inflearnspringbootbeginner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSpringBootBeginnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflearnSpringBootBeginnerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kamang/inflearnspringbootbeginner/InflearnSpringBootBeginnerApplicationTests.java
@@ -0,0 +1,13 @@
+package kamang.inflearnspringbootbeginner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnSpringBootBeginnerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
